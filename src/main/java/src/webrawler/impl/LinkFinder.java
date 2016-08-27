e2e088f585806783218fbdf78ac89d287d522d94
vijay <krishna2642@gmail.com>
2016-08-27 17:14:12 -0500

    Initial commit

---
+++ src/main/java/src/webrawler/impl/LinkFinder.java
@@ -0,0 +1,82 @@
+package webrawler.impl;
+
+import org.apache.commons.validator.routines.UrlValidator;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import webrawler.LinkHandler;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by vijay on 8/18/16.
+ */
+public class LinkFinder implements Runnable{
+
+    private String url;
+    private WebCrawler handler;
+    private static final Long t0 = System.nanoTime();
+
+    public LinkFinder(String url, LinkHandler handler){
+        this.url = url;
+        this.handler = (WebCrawler) handler;
+
+    }
+
+    @Override
+    public void run() {
+        getSimpleLinks(url);
+    }
+
+    private void getSimpleLinks(String url){
+        //if not already visited
+        if (!handler.visited(url)) {
+            try {
+                UrlValidator validator =new UrlValidator();
+                List<String> urls = new ArrayList<String>();
+                //if ( handler.size() <=1500) {
+                    Document doc = Jsoup.parse(new URL(url), 3000);
+                    Elements elements = doc.getElementsByTag("a");
+                    for (Element element : elements
+                            ) {
+                        String link = element.attr("href").trim();
+                        if(link.startsWith("//")){
+                            link = "https:"+link;
+                        }
+                        if (validator.isValid(link) && !handler.visited(link) && !handler.visitedLinks.contains(link) && link.contains(handler.getSiteName())) {
+
+                            urls.add(link);
+                        }
+                    }
+                //}
+                //we visited this url
+                handler.removeVisiting(url);
+                handler.addVisited(url);
+                System.out.println("visiting links size "+ handler.visitingLinks.size() +" visited links : "+  handler.visitedLinks.size());
+                for (String l : urls) {
+                    handler.queueLink(l);
+                }
+
+                /*if (handler.size() >= 1500) {
+                    System.out.println("Time to visit "+handler.size()+"distinct links = " + (System.nanoTime() - t0));
+                    System.out.println("visiting links size "+ handler.visitingLinks.size());
+                    handler.removeVisiting(url);
+                    return;
+                }else {
+                    handler.removeVisiting(url);
+                    handler.addVisited(url);
+                    System.out.println("visiting links size "+ handler.visitingLinks.size() +" visited links : "+  handler.visitedLinks.size());
+                    for (String l : urls) {
+                        handler.queueLink(l);
+                    }
+                }*/
+
+            } catch (Exception e) {
+                //ignore all errors for now
+            }
+        }
+    }
+}
---
+++ src/main/java/src/webrawler/impl/WebCrawler.java
@@ -0,0 +1,87 @@
+package webrawler.impl;
+
+import webrawler.LinkHandler;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/**
+ * Created by vijay on 8/18/16.
+ */
+public class WebCrawler implements LinkHandler{
+    public final Collection<String> visitedLinks = Collections.synchronizedSet(new HashSet<String>());
+    public final Collection<String> visitingLinks = Collections.synchronizedSet(new HashSet<String>());
+
+    private String url;
+    private String siteName;
+
+    private ExecutorService executorService;
+
+    public WebCrawler(String url, int maxThreads){
+        this.url = url;
+        executorService = Executors.newFixedThreadPool(maxThreads);
+
+    }
+    @Override
+    public void queueLink(String link) {
+        //if(this.size()%100 ==0){
+            //System.out.println("Size : "+ this.size()+" visiting "+ link);
+        //}
+        if( !visitedLinks.contains(link) && !visitingLinks.contains(link)) {
+            visitingLinks.add(link);
+            this.startNewThread(link);
+        }
+
+        if (visitedLinks.size() ==0){
+            executorService.shutdown();
+            System.out.println("Shutting down executor service!!");
+
+            visitedLinks.clear();
+            visitingLinks.clear();
+        }
+    }
+
+    @Override
+    public int size() {
+        return visitedLinks.size();
+    }
+
+    @Override
+    public boolean visited(String link) {
+        return visitedLinks.contains(link);
+    }
+
+    @Override
+    public void addVisited(String url) {
+        visitedLinks.add(url);
+    }
+
+    @Override
+    public void removeVisiting(String url) {
+        visitingLinks.remove(url);
+    }
+
+    private void startNewThread(String link){
+        executorService.execute(new LinkFinder(link, this));
+    }
+
+    public void startCrawling() {
+        this.startNewThread(this.url);
+    }
+
+    public static void main(String [] args){
+        WebCrawler crawler =  new WebCrawler("https://te.wikipedia.org/wiki/", 10);
+        crawler.setSiteName("te.wikipedia");
+        crawler.startCrawling();
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+}
